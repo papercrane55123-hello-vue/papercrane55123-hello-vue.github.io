90b0a3cb7438d84b383aced9fc31cd03e9115fef
papercrane55123 <gyeongmin55@naver.com>
2024-01-31 12:09:44 +0900

    var

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+// hello world
+import { ref } from 'vue'
+const message = ref({name: "MIN", age:28})
+const name = "KIN"
+</script>
+
+<template>
+ <h1>{{ message }}</h1>
+</template>
+
+

9b6589f67f1c054ceb3c4169b140a2272aad1fad
papercrane55123 <gyeongmin55@naver.com>
2024-01-31 14:35:24 +0900

    clear

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 	import {ref} from 'vue'
+	import category from "@/assets/category.json"
+	const [one, two, three, four] = category
+	const categories = [one, two, three, four]
 	const parentMessage = ref('Parent')
 	const items = ref([{ message: 'Foo', name: 'min' }, {message: 'Bar'}])
 </script>
@@ -7,5 +10,9 @@
 <template>
 	<li v-for = "(item, index) in items">
 		{{ item.name }} - {{ index }} - {{ item.message }}
+	</li>
+
+	<li v-for = "(ctr) in categories">
+		{{ ctr.name }} - {{ ctr.code }}
 	</li>
 </template>

961ebc30045847deca669559d91877dd4f36b2da
papercrane55123 <gyeongmin55@naver.com>
2024-01-31 12:57:55 +0900

    exit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const awesome = ref(true)
+function toggleAwesome(v){
+	awesome.value = !v
+}
+
+function isAwesome(){
+ return awesome.value
+}
+</script>
+
+
+<template>
+ <button @click="toggleAwesome(awesome)">toggle</button>
+	<h1 v-if="isAwesome()">Vue is awesome!</h1>
+	<h1 v-else>Oh no 😰</h1>
+
+	<h1 v-if="isAwesome">v-if True</h1>
+	<h1 v-show="isAwesome()">v-show True</h1>
+
+</template>

2f0dcb4c80fbc8966b53dfc49dfe35c3138895bd
papercrane55123 <gyeongmin55@naver.com>
2024-02-02 11:18:22 +0900

    moringStretch

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <script setup>
 import { ref } from 'vue'
 
-let id = 0
-const newTodo = ref('')
-const todos = ref([
-	{ id: id++, issue: 'Backlog' }, { id: id++, issue: 'Todo' }, 
-	{ id: id++, issue: 'Going Hawaii' }
-])
-function addTodo(){
-	console.log(newTodo.value)
-	todos.value.push({ id: id++, issue: newTodo.value })
-	newTodo.value = ''	
+let show = ref(true)
+const list = ref([1,2,3])
+
+function landering(){
+	console.log(show.value)
+	if(show.value === true){
+		show.value = false
+	} else { show.value = true }
 }
-function removeTodo(todo){
-	console.log(todo)
-	todos.value = todos.value.filter((t) => t !== todo)
+function addList(){
+	let i = list.value.length + 1 
+	list.value.push(i)
+}
+function deleteList(){
+	list.value.splice(list.value.length-1)
+}
+function reverseList(){
+	list.value.reverse()
 }
 </script> 
 
 <template>
-	<form @submit.prevent="addTodo">
-		<input v-model="newTodo">
-		<button>할 일 추가</button>
-	</form>
-	<ul>
-		<li v-for="todo in todos">
-			{{ todo.issue }} - <button @click="removeTodo(todo)">Done</button>
-		</li>
+	<button @click.prevent="landering">List 랜더링 ON/Off </button>
+	<button @click.prevent="addList">List 추가 </button>
+	<button @click.prevent="deleteList">List 제거 </button>
+	<button @click.prevent="reverseList">List 뒤집기</button>
+	
+	<ul v-if="list.length > 0 && show">
+		<li v-for="item of list">{{ item }} </li>
 	</ul>
+	<p v-else-if="list.length > 0">List is not empty, but hidden</p>
+	<p v-else>List is empty</p>
 </template>
